40dac181149fd3ef3b9fbe966a3cc143e30e4b9b
mahalakshme <mahalakshme1994@gmail.com>
2015-01-10 10:55:45 +0530

    mahalakshme:First Commit

---
+++ src/com/twu/biblioteca/Biblioteca.java
@@ -0,0 +1,12 @@
+package com.twu.biblioteca;
+
+/**
+ * Created by mahalaks on 10/01/15.
+ */
+public class Biblioteca {
+
+    public Biblioteca()
+    {
+        System.out.print("Welcome");
+    }
+}
---
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -0,0 +1,9 @@
+package com.twu.biblioteca;
+
+
+public class BibliotecaApp {
+
+    public static void main(String[] args) {
+       Biblioteca myLibrary = new Biblioteca();
+    }
+}
