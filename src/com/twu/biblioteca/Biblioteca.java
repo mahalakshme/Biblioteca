2557f044d0424f270eeb32efe3df7069fd753591
mahalakshme <mahalakshme1994@gmail.com>
2015-01-10 12:40:08 +0530

    mahalakshme:Second Commit

--- src/com/twu/biblioteca/Biblioteca.java
+++ src/com/twu/biblioteca/Biblioteca.java
@@ -1,12 +1,30 @@
 package com.twu.biblioteca;
 
+import java.util.ArrayList;
+
 /**
  * Created by mahalaks on 10/01/15.
  */
 public class Biblioteca {
 
+    ArrayList<String> menu = new ArrayList<String>();
+
+
     public Biblioteca()
     {
         System.out.print("Welcome");
+        CreateMenu();
+    }
+
+    private void CreateMenu() {
+        menu.add("List Books");
+        menu.add("Checkout Book");
+    }
+
+    public void DisplayMenu() {
+        for (String option : menu) {
+            System.out.println(option);
+        }
+
     }
 }
